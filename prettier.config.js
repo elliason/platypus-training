3e1c1a154a21e4391343b9095aacd378c15b79ac
Ondrej Elias <ondrej.elias@mediafactory.cz>
2024-04-20 12:06:19 +0200

    init web app

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+    semi: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    tabWidth: 4,
+    printWidth: 120,
+};
+
+export default config;
